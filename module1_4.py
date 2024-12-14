9e9610ed3169f57e5d2be37f9abd9f24ea6179dd
Wanderer134 <danilgusev981@gmail.com>
2024-12-14 11:35:20 +0300

    Update module3_4.py

--- module1_4.py
+++ module1_4.py
@@ -5,11 +5,11 @@ print_params (b=25)
 print_params(c=[1,2,3])
 
 
-values_list = (50,'paper', False)
+values_list = [0,'paper', False]
 values_dict = {'a': 1, 'b': 'string', 'c': True }
 print_params(*values_list)
 print_params(**values_dict)
 
 
-values_list_2 = (69, True)
+values_list_2 = [69, True]
 print_params(*values_list_2, 42)
